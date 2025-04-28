0852362e9af017aae7c3c6b4fc19bcd91a942694
duong <isdon243@gmail.com>
2025-04-29 06:37:24 +0700

    start-point

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
-
-import "./App.scss";
-
-// const App = () => {
-//   return (
-//     <div
-//     //  class="app-container"
-//     >
-//       {/* Hlloe with & ok */}
-//       <MyComponent />
-//     </div>
-//   );
-// };
+import logo from './logo.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 const App = () => {
-
+  const count = useSelector(state => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
-
     <div className="App">
-        Hello World
-    </div>  
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>Count = {count}</div>
+        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+      </header>
+    </div>
   );
 }
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           hekko?
-
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
